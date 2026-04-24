ce5ba08657d44eea33bf7ba8e32e2c17e638afc7
zizars <151849313+zizars@users.noreply.github.com>
2026-04-24 17:44:46 -0400

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -4,5 +4,5 @@ const app = require("../app");
 test("GET / responde Hola Mundo", async () => {
   const res = await request(app).get("/");
   expect(res.statusCode).toBe(200);
-  expect(res.text).toBe("Hola Mundo DevOps ");
+  expect(res.text).toBe("Hola Mundo DevOps 🚀");
 });
