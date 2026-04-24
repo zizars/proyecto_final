7f4256427f6c7ed06d0a8235b6af6c9d6f5f30b1
zizars <151849313+zizars@users.noreply.github.com>
2026-04-24 17:09:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hola Mundo DevOps 🚀");
+});
+
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(3000, () => console.log("Server corriendo en puerto 3000"));
+}
