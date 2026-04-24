b15cff20226b6408cd40110d63bb9e4736062cd1
zizars <151849313+zizars@users.noreply.github.com>
2026-04-24 17:12:27 -0400

    Update and rename app.test.json to app.test.js

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const app = require("../app");
+
+test("GET / responde Hola Mundo", async () => {
+  const res = await request(app).get("/");
+  expect(res.statusCode).toBe(200);
+  expect(res.text).toBe("Hola Mundo DevOps ");
+});
